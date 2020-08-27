52da6b58f6a39b0734638a4014243f0d5bf45c80
Yang Gu <yangu@microsoft.com>
2020-08-27 09:27:07 +0800

    Preserve old conda packages when new fail to generate

--- prune.sh
+++ prune.sh
@@ -9,6 +9,24 @@ grab_version() {
         xargs
 }
 
+grab_latest_version() {
+    conda search -c "${CHANNEL}" --platform "${platform}" "${PKG}" 2>/dev/null | \
+        grep "${CHANNEL}" | \
+        awk -F '  *' '{print $2}' | \
+        uniq | \
+        tail -n 1 | \
+        xargs
+}
+
+grab_builds() {
+    conda search -c "${CHANNEL}" --platform "${platform}" "${PKG}" 2>/dev/null | \
+        grep "${CHANNEL}" | \
+		grep "$1" | \
+        awk -F '  *' '{print $3}' | \
+		uniq | \
+        xargs
+}
+
 # TODO: Write this in a proper programming language
 
 set -eou pipefail
@@ -17,21 +35,19 @@ CHANNEL=${CHANNEL:-pytorch-nightly}
 PKG=${PKG:-pytorch}
 PLATFORMS=${PLATFORMS:-noarch osx-64 linux-64 win-64}
 
-# Will grab all versions except for the latest one,
-versions_to_prune=()
 for platform in ${PLATFORMS}; do
-    versions_to_prune+=( $(grab_version || true) )
-done
-
-declare -A unique_versions
-# Only use unique versions
-for version in ${versions_to_prune[@]}; do
-    unique_versions["${version}"]=""
-done
-
-for version in ${!unique_versions[@]}; do
-    (
-        set -x
-        anaconda remove --force ${CHANNEL}/${PKG}/${version}
-    )
+    latest_version="$(grab_latest_version || true)"
+	latest_builds="$(grab_builds "${latest_version}" || true)"
+    versions_to_prune="$(grab_version || true)"
+	for version in ${versions_to_prune}; do
+		builds="$(grab_builds "${version}" || true)"
+		for build in ${builds}; do
+		    if [[ "${latest_builds}" =~ "${build}" ]];then
+			    (
+			        set -x
+			        anaconda remove --force ${CHANNEL}/${PKG}/${version}/${PLATFORMS}/${PKG}-${version}-${build}.tar.bz2
+				)
+			fi
+        done
+    done
 done
